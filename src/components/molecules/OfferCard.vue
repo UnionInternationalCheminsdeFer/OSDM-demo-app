0d1aa223c7715a48de3fd91bd49c0179c079807a
TheNewCivilian <git@prenninger.de>
2025-03-14 14:27:20 +0100

    Fix type check

--- src/components/molecules/OfferCard.vue
+++ src/components/molecules/OfferCard.vue
@@ -42,7 +42,7 @@
       <div class="w-full flex justify-end gap-14 items-center">
         <span class="text-lg font-bold">{{
           displayPrice(
-            extractPriceFromOffer(offer),
+            extractPriceFromOffer(offer as components['schemas']['Offer']),
             addedAncillaries.map((aa) => aa.price),
           )
         }}</span>
